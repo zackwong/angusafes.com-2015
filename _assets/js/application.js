024ec831cb8df20b273f080cb3e0d09414ad8636
zackwong <hzzzoo@126.com>
2019-05-04 01:16:54 +0800

    up

--- _assets/js/application.js
+++
@@ -1,6 +0,0 @@
-//= require jquery.min.js
-//= require jquery.slides.min.js
-//= require common.js
-//= require jquery.mousewheel-3.0.6.pack.js
-//= require autoimg.js
-//= require jquery.fancybox.pack.js
